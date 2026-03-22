29f2d59d3691fd46e0bcab0d5b486180eb1cd043
predator522 <p47017988@gmail.com>
2026-03-22 08:12:16 +0000

    Create Home.js

---
+++ pages/Home.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import { db } from "../firebase";
+import { collection, getDocs } from "firebase/firestore";
+import { Link } from "react-router-dom";
+
+function Home() {
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    const fetchServices = async () => {
+      const data = await getDocs(collection(db, "services"));
+      setServices(data.docs.map(doc => ({ ...doc.data(), id: doc.id })));
+    };
+    fetchServices();
+  }, []);
+
+  return (
+    <div>
+      <h1>DevMart</h1>
+
+      {services.map(service => (
+        <div key={service.id} style={{border: "1px solid #ccc", margin: 10, padding: 10}}>
+          <h3>{service.title}</h3>
+          <p>₵{service.price}</p>
+          <Link to={`/service/${service.id}`}>View</Link>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Home;
