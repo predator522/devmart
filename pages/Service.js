0ba26f93ea2b27b066fd557165dbe5717e5aaa6d
predator522 <p47017988@gmail.com>
2026-03-22 08:13:10 +0000

    Update Service.js

--- pages/Service.js
+++ pages/Service.js
@@ -1,1 +1,36 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { db } from "../firebase";
+import { doc, getDoc } from "firebase/firestore";
 
+function Service() {
+  const { id } = useParams();
+  const [service, setService] = useState(null);
+
+  useEffect(() => {
+    const fetchService = async () => {
+      const docRef = doc(db, "services", id);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        setService(docSnap.data());
+      }
+    };
+
+    fetchService();
+  }, [id]);
+
+  if (!service) return <p>Loading...</p>;
+
+  return (
+    <div>
+      <h2>{service.title}</h2>
+      <p>₵{service.price}</p>
+      <p>{service.desc}</p>
+
+      <button>Buy Now</button>
+    </div>
+  );
+}
+
+export default Service;
